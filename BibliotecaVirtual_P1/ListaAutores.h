9aafeb4d076ed1344d6fe0fca8fcb0cf4e6347ab
Guayanay Lema Erika Vannesa <150321918+GUAYANAY@users.noreply.github.com>
2024-12-07 01:28:16 -0500

    Add files via upload

---
+++ BibliotecaVirtual_P1/ListaAutores.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "NodoAutores.h"
+#include <string>
+#include <fstream>
+#include "json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+class ListaAutores {
+private:
+    NodoAutores* cabeza;
+
+public:
+    ListaAutores();
+    ~ListaAutores();
+
+    bool validarCedulaUnica(string cedula);
+
+    bool insertar(string cedula, string nombre, string apellido, string fechaPublicacion);
+    NodoAutores* buscar(string cedula);
+    bool eliminar(string cedula);
+    void mostrar();
+
+    // Nuevas funciones para JSON
+    void cargarDesdeArchivoJSON(); // Cargar datos desde un archivo JSON
+    void guardarEnArchivoJSON();   // Guardar datos en un archivo JSON
+};
