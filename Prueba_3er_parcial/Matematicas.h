dee9497a7be4d80dd6040933f28713463c5a35ed
Guayanay Lema Erika Vannesa <150321918+GUAYANAY@users.noreply.github.com>
2025-02-28 19:15:31 -0500

    Update Matematicas.h

--- Prueba_3er_parcial/Matematicas.h
+++ Prueba_3er_parcial/Matematicas.h
@@ -1,3 +1,12 @@
+/********************************************************************************************
+ *            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE                                       *
+ *                                                                                          *
+ * Autor:                          Erika Guayanay, Maycol Celi, Jerson Llumiquinga          *
+ * Fecha de creacion:              28/02/2025                                               *
+ * Fecha de modificacion:          25/02/2025                                               *
+ * Materia:                        Estructura de datos                                      *
+ * NRC :                           1992                                                     *
+ ********************************************************************************************/
 #pragma once
 #include <stdexcept>
 #include <iostream>
